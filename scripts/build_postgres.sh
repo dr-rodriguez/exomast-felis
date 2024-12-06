e20378cd110868c17f8075b7896fa987a0159a2e
David Rodriguez <drodriguez@stsci.edu>
2024-12-06 14:34:02 -0500

    adding schema sql

--- scripts/build_postgres.sh
+++ scripts/build_postgres.sh
@@ -6,4 +6,4 @@ felis validate --check-description exomast/schema.yaml
 
 # Use --initialize or --drop to create a brand new database, otherwise it will update the existing one
 felis create --drop --engine-url postgresql+psycopg2://postgres:password@localhost:5432/exomast exomast/schema.yaml
-# felis create --engine-url postgresql+psycopg2://postgres:password@localhost:5432/exomast exomast/schema.yaml
+# felis create --engine-url postgresql+psycopg2://postgres:password@localhost:5432/exomast exomast/schema.yaml --output-file exomast/schema.sql

84c98009722176ee00f2682a57ff1a569850cdad
David Rodriguez <drodriguez@stsci.edu>
2024-12-12 11:13:07 -0500

    reworking after astrodbkit improvements

--- scripts/build_postgres.py
+++ scripts/build_postgres.py
@@ -1,35 +1,37 @@
 # Python script to build the database from the shema.yaml file
 
-import yaml
-import sqlalchemy as sa
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.schema import CreateSchema
+# import yaml
+# import sqlalchemy as sa
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import Session
+# from sqlalchemy.ext.automap import automap_base
+# from sqlalchemy.exc import IntegrityError
+# from sqlalchemy.schema import CreateSchema
 
-from felis.datamodel import Schema
-from felis.metadata import MetaDataBuilder
-from felis.db.utils import DatabaseContext
+# from felis.datamodel import Schema
+# from felis.metadata import MetaDataBuilder
+# from felis.db.utils import DatabaseContext
+from astrodbkit.astrodb import create_database
 
 CONNECTION_STRING = "postgresql+psycopg2://postgres:password@localhost:5432/exomast"
+SCHEMA_PATH = "exomast/schema.yaml"
+
+# AstrodbKit version
+create_database(connection_string=CONNECTION_STRING, felis_schema=SCHEMA_PATH)
+
+# More manual approach:
 
 # Load and validate schema file
-data = yaml.safe_load(open("exomast/schema.yaml", "r"))
-schema = Schema.model_validate(data)
+# data = yaml.safe_load(open(SCHEMA_PATH, "r"))
+# schema = Schema.model_validate(data)
 
- # Create database from Felis schema
-metadata = MetaDataBuilder(schema).build()
+#  # Create database from Felis schema
+# metadata = MetaDataBuilder(schema).build()
 
-# Create the database
-engine = create_engine(CONNECTION_STRING)
-schema_name = "exomast"
-with engine.connect() as connection:
-    connection.execute(CreateSchema(schema_name, if_not_exists=True))
-    connection.commit()
-metadata.create_all(engine)
-
-# Not sure if the below works
-# ctx = DatabaseContext(metadata, engine)
-# ctx.initialize()
-# ctx.create_all()
+# # Create the database
+# engine = create_engine(CONNECTION_STRING)
+# schema_name = "exomast"
+# with engine.connect() as connection:
+#     connection.execute(CreateSchema(schema_name, if_not_exists=True))
+#     connection.commit()
+# metadata.create_all(engine)
--- scripts/ingest_sample.py
+++ scripts/ingest_sample.py
@@ -1,19 +1,10 @@
 # Python script to populate database
 
-import os
-import pytest
-import yaml
 import sqlalchemy as sa
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.exc import IntegrityError
-
-from felis.datamodel import Schema
-from felis.metadata import MetaDataBuilder
-
 from astrodbkit.astrodb import Database
-
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
 
 CONNECTION_STRING = "postgresql+psycopg2://postgres:password@localhost:5432/exomast"
 # CONNECTION_STRING = "sqlite:///exomast.sqlite"
@@ -23,31 +14,43 @@ REFERENCE_TABLES = [
 ]
 
 # Check if DB/tables exists
-engine = create_engine(CONNECTION_STRING, connect_args={'options': '-csearch_path={}'.format("exomast")})
+engine = create_engine(CONNECTION_STRING, connect_args={"options": "-csearch_path=exomast"})
 with Session(engine) as session:
     # session.execute(sa.text("SET search_path TO exomast"))  # set search path to use the schema
-    result = session.execute(sa.text('select * from "Sources"')).fetchall()
+    result = session.execute(sa.text('select * from "Sources"')).fetchall()  # need quotes for case sensitivity
 print(result)
 
-db = Database(CONNECTION_STRING, 
-              reference_tables=REFERENCE_TABLES, 
-              connection_arguments={'options': '-csearch_path=exomast'})
-
-# Create object for SQLAlchemy use
-Sources = db.metadata.tables["Sources"]
+db = Database(CONNECTION_STRING, reference_tables=REFERENCE_TABLES, schema="exomast")
 
 # Data to be loaded, as list of dictionaries
 source_data = [
     {
-        "id": 1, 
-        "type": "exoplanet", 
-        "survey": "nexsci", 
+        "id": 1,
+        "type": "exoplanet",
+        "survey": "nexsci",
         "name": "HAT-P-11 b",
     },
 ]
 
 # Actual ingest of data
-with engine.connect() as conn:
-    conn.execute(Sources.insert().values(source_data))
+with db.engine.connect() as conn:
+    conn.execute(db.Sources.insert().values(source_data))
     conn.commit()
 
+# ORM example
+# Fails due to backref issue; maybe something related to the foreign keys:
+# sqlalchemy.exc.ArgumentError: Error creating backref 'sources_collection' on relationship 'Sources.sources_collection': property of that name exists on mapper 'Mapper[Sources(Sources)]'
+# Base = automap_base(metadata=db.metadata)
+# Base.prepare()
+
+# # Creating the actual Table objects
+# Sources = Base.classes.Sources
+
+# # Adding and removing a basic source
+# s = Sources(id=2, name="V4046 Sgr", survey="None", type="star")
+# with db.session as session:
+#     session.add(s)
+#     session.commit()
+
+t = db.query(db.Sources).table()
+print(t)

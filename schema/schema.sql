339894c84cfd121d11e70c756c391fad3f1006e2
David Rodriguez <drodriguez@stsci.edu>
2024-12-17 14:43:14 -0500

    schema update to add Publications and Properties table

--- schema/schema.sql
+++ schema/schema.sql
@@ -15,6 +15,19 @@ COMMENT ON COLUMN exomast."Sources".type IS 'Type of source (eg, exoplanet, brow
 COMMENT ON COLUMN exomast."Sources".survey IS 'Originating survey (eg, nexsci, TOI, etc)';
 COMMENT ON COLUMN exomast."Sources".name IS 'Primary name for this source from survey';
 
+CREATE TABLE exomast."Publications" (
+	id BIGSERIAL, 
+	reference VARCHAR(100) NOT NULL, 
+	ref VARCHAR(30), 
+	PRIMARY KEY (id)
+)
+
+;
+COMMENT ON TABLE exomast."Publications" IS 'Publications/references for values in database';
+COMMENT ON COLUMN exomast."Publications".id IS 'Publication internal identifier';
+COMMENT ON COLUMN exomast."Publications".reference IS 'Publication reference name (eg, Author, et al. 2021)';
+COMMENT ON COLUMN exomast."Publications".ref IS 'Publication bibcode';
+
 CREATE TABLE exomast."Matches" (
 	id1 BIGINT, 
 	id2 BIGINT, 
@@ -39,7 +52,7 @@ CREATE TABLE exomast."Names" (
 
 ;
 COMMENT ON TABLE exomast."Names" IS 'Additional identifiers for objects in Sources table';
-COMMENT ON COLUMN exomast."Names".id IS 'Main identfier for an object; links to Sources table';
+COMMENT ON COLUMN exomast."Names".id IS 'Main identifier for an object; links to Sources table';
 COMMENT ON COLUMN exomast."Names".name IS 'Identifier for source';
 COMMENT ON CONSTRAINT "Names_id_Sources_id" ON exomast."Names" IS 'Link Names to Sources table';
 
@@ -58,19 +71,46 @@ COMMENT ON TABLE exomast."Coords" IS 'Coordinates for ExoMAST objects';
 COMMENT ON COLUMN exomast."Coords".id IS 'Main source identifier';
 COMMENT ON COLUMN exomast."Coords".ra IS 'ICRS Right Ascension of object';
 COMMENT ON COLUMN exomast."Coords".dec IS 'ICRS Declination of object';
-COMMENT ON CONSTRAINT check_dec ON exomast."Coords" IS 'Validate Dec range';
 COMMENT ON CONSTRAINT "Coords_id_Sources_id" ON exomast."Coords" IS 'Link Coords to Sources table';
 COMMENT ON CONSTRAINT check_ra ON exomast."Coords" IS 'Validate RA range';
+COMMENT ON CONSTRAINT check_dec ON exomast."Coords" IS 'Validate Dec range';
 
 CREATE TABLE exomast."PlanetProperties" (
 	id BIGINT, 
 	orbital_period FLOAT, 
+	orbital_period_error FLOAT, 
+	orbital_period_ref BIGINT, 
 	PRIMARY KEY (id), 
-	CONSTRAINT "PlanetProperties_id_Sources_id" FOREIGN KEY(id) REFERENCES exomast."Sources" (id)
+	CONSTRAINT "PlanetProperties_id_Sources_id" FOREIGN KEY(id) REFERENCES exomast."Sources" (id), 
+	CONSTRAINT "PlanetProperties_id_Publications_id" FOREIGN KEY(orbital_period_ref) REFERENCES exomast."Publications" (id)
 )
 
 ;
 COMMENT ON TABLE exomast."PlanetProperties" IS 'Properties for ExoMAST planets';
 COMMENT ON COLUMN exomast."PlanetProperties".id IS 'Main source identifier';
 COMMENT ON COLUMN exomast."PlanetProperties".orbital_period IS 'Orbital period in days';
+COMMENT ON COLUMN exomast."PlanetProperties".orbital_period_error IS 'Uncertainty of orbital period in days';
+COMMENT ON COLUMN exomast."PlanetProperties".orbital_period_ref IS 'Publication identifier for orbital period';
+COMMENT ON CONSTRAINT "PlanetProperties_id_Publications_id" ON exomast."PlanetProperties" IS 'Link PlanetProperties to Publications table';
 COMMENT ON CONSTRAINT "PlanetProperties_id_Sources_id" ON exomast."PlanetProperties" IS 'Link PlanetProperties to Sources table';
+
+CREATE TABLE exomast."Properties" (
+	id BIGINT, 
+	property_type VARCHAR(30), 
+	property_value FLOAT, 
+	property_error FLOAT, 
+	property_reference BIGINT, 
+	PRIMARY KEY (id, property_type), 
+	CONSTRAINT "Properties_id_Sources_id" FOREIGN KEY(id) REFERENCES exomast."Sources" (id), 
+	CONSTRAINT "Properties_id_Publications_id" FOREIGN KEY(property_reference) REFERENCES exomast."Publications" (id)
+)
+
+;
+COMMENT ON TABLE exomast."Properties" IS 'Properties for ExoMAST planets; long-form version';
+COMMENT ON COLUMN exomast."Properties".id IS 'Main source identifier';
+COMMENT ON COLUMN exomast."Properties".property_type IS 'Property type (eg, the name "orbital period")';
+COMMENT ON COLUMN exomast."Properties".property_value IS 'Value of property';
+COMMENT ON COLUMN exomast."Properties".property_error IS 'Uncertainty of value';
+COMMENT ON COLUMN exomast."Properties".property_reference IS 'Publication reference';
+COMMENT ON CONSTRAINT "Properties_id_Publications_id" ON exomast."Properties" IS 'Link Properties to Publications table';
+COMMENT ON CONSTRAINT "Properties_id_Sources_id" ON exomast."Properties" IS 'Link Properties to Sources table';

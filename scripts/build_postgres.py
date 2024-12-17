f3c85c76708b03cb2a97f0506ca2a3d25759405d
David Rodriguez <drodriguez@stsci.edu>
2024-12-17 11:12:15 -0500

    schema fix and implementing handling of TAP_SCHEMA

--- scripts/build_postgres.py
+++ scripts/build_postgres.py
@@ -1,23 +1,42 @@
 # Python script to build the database from the shema.yaml file
 
-# import yaml
-# import sqlalchemy as sa
-# from sqlalchemy import create_engine
+import yaml
+import sqlalchemy as sa
+from sqlalchemy import create_engine
 # from sqlalchemy.orm import Session
 # from sqlalchemy.ext.automap import automap_base
 # from sqlalchemy.exc import IntegrityError
-# from sqlalchemy.schema import CreateSchema
+from sqlalchemy.schema import CreateSchema
 
 # from felis.datamodel import Schema
 # from felis.metadata import MetaDataBuilder
 # from felis.db.utils import DatabaseContext
-from astrodbkit.astrodb import create_database
+from astrodbkit.astrodb import create_database, Database
 
 CONNECTION_STRING = "postgresql+psycopg://postgres:password@localhost:5432/exomast"
 SCHEMA_PATH = "exomast/schema.yaml"
+DELETE_SCHEMA = True
 
-# AstrodbKit version
+# Get schema name
+data = yaml.safe_load(open(SCHEMA_PATH, "r"))
+SCHEMA_NAME = data["name"]
+
+# Clear database/schema if requested
+if DELETE_SCHEMA:
+    engine = create_engine(CONNECTION_STRING)
+    with engine.connect() as conn:
+        conn.execute(sa.text(f"DROP SCHEMA {SCHEMA_NAME} CASCADE;"))
+        conn.commit()
+
+# AstrodbKit version of creating and connecting to the database
 create_database(connection_string=CONNECTION_STRING, felis_schema=SCHEMA_PATH)
+
+# Create TAP_SCHEMA for later use
+db = Database(connection_string=CONNECTION_STRING, schema=SCHEMA_NAME)
+with db.engine.connect() as conn:
+    conn.execute(CreateSchema("TAP_SCHEMA", if_not_exists=True))
+    conn.commit()
+
 
 # More manual approach:
 
--- scripts/build_postgres.sh
+++ scripts/build_postgres.sh
@@ -5,5 +5,11 @@
 felis validate --check-description exomast/schema.yaml
 
 # Use --initialize or --drop to create a brand new database, otherwise it will update the existing one
+# May want to manually drop with: DROP SCHEMA exomast CASCADE;
 # felis create --drop --engine-url postgresql+psycopg://postgres:password@localhost:5432/exomast exomast/schema.yaml
 felis create --engine-url postgresql+psycopg://postgres:password@localhost:5432/exomast exomast/schema.yaml --output-file exomast/schema.sql
+
+# TAP metadata initialization and load
+# Requires the schema to be created first and specified in the call
+felis init-tap postgresql+psycopg://postgres:password@localhost:5432/exomast --tap-schema-name=TAP_SCHEMA
+felis load-tap --engine-url=postgresql+psycopg://postgres:password@localhost:5432/exomast --tap-schema-name=TAP_SCHEMA exomast/schema.yaml

16813d08c6a14cd6fb94d4f444b51f2a8647c9a7
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2024-12-21 18:16:47 -0400

    adding helper script to clear database table content

---
+++ scripts/clear_db.py
@@ -0,0 +1,26 @@
+# Helper script to clear all database tables (not DROP)
+
+import sqlalchemy as sa
+import yaml
+from config import REFERENCE_TABLES, CONNECTION_STRING, SCHEMA_PATH, SCHEMA_NAME
+from astrodbkit.astrodb import Database
+
+db = Database(CONNECTION_STRING, reference_tables=REFERENCE_TABLES, schema=SCHEMA_NAME)
+
+# Get table names
+data = yaml.safe_load(open(SCHEMA_PATH, "r"))
+table_data = data.get("tables")
+table_names = [x.get("name") for x in table_data]
+print(table_names)
+
+for name in table_names:
+    if name in ["Sources", "Publications"]:
+        # Truncate Sources and Publications last
+        continue
+    db.session.execute(sa.text(f'TRUNCATE "{name}" CASCADE;'))
+
+# Final tables
+db.session.execute(sa.text('Truncate "Sources" CASCADE;'))
+db.session.execute(sa.text('Truncate "Publications" CASCADE;'))
+
+print("All tables cleared")
---
+++ scripts/config.py
@@ -0,0 +1,9 @@
+# Goblal variables
+
+SCHEMA_PATH = "schema/schema.yaml"
+SCHEMA_NAME = "exomast"
+CONNECTION_STRING = "postgresql+psycopg://postgres:password@localhost:5432/exomast"
+REFERENCE_TABLES = [
+    "Publications",
+    "Surveys",
+]

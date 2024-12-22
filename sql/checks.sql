31ba93e077e244757edf68581536cb4e49095913
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2024-12-21 21:40:06 -0400

    adding a few more sql examples

--- sql/checks.sql
+++ sql/checks.sql
@@ -1,5 +1,6 @@
 
 -- Examine results
+select COUNT(*) from exomast."Sources";
 select * from exomast."Sources" limit 1000;
 select * from exomast."PlanetProperties" LIMIT 1000;
 
@@ -11,3 +12,9 @@ LIMIT 1000;
 
 -- Check duplicates in Sources
 select survey, primary_name, COUNT(*) from exomast."Sources" GROUP BY survey, primary_name HAVING count(*) > 1
+
+-- Single planet search
+select * from exomast."Names" as N where N.name in ('Kepler-1498 b');
+select * from exomast."Sources" where id in (
+    select id from exomast."Names" as N where N.name in ('Kepler-1498 b')
+);

5786bd67275448961b782f26c52fdf6b02be2355
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2024-12-22 19:49:29 -0400

    working sql function

--- sql/build_functions.sql
+++ sql/build_functions.sql
@@ -1,39 +1,30 @@
 -- Script to build functions
 
+DROP FUNCTION IF EXISTS exomast.get_planet_info(planet_name text);
+
 CREATE OR REPLACE FUNCTION exomast.get_planet_info(planet_name TEXT)
 RETURNS TABLE(
-    id INTEGER,
-    survey TEXT,
-    primary_name TEXT,
+    id BIGINT,
+    survey VARCHAR(30),
+    primary_name VARCHAR(30),
     ra FLOAT,
-    "dec" FLOAT,
+    decl FLOAT,
     orbital_period FLOAT,
-    orbital_period_error FLOAT,
+    orbital_period_error FLOAT
 ) AS $$
-DECLARE
-    matched_ids INTEGER[];
-    rec RECORD;
 BEGIN
-    -- Get the IDs of matching planets
-    SELECT ARRAY(
-        SELECT DISTINCT(M.id2) FROM exomast."Matches" AS M
-        JOIN exomast."Names" AS N ON M.id1=N.id
-        WHERE N.name = planet_name) INTO matched_ids;
-
-    -- Check if any IDs were found.
-    IF matched_ids IS NULL OR array_length(matched_ids, 1) IS NULL THEN
-        RETURN QUERY SELECT NULL AS id, NULL AS survey, NULL AS primary_name, 
-        NULL AS ra, NULL AS "dec", 
-        NULL AS orbital_period, NULL AS orbital_period_error; -- Return dummy row if no matches
-    END IF;
-
-    -- Join tables and return data
     RETURN QUERY
     SELECT s.id, s.survey, s.primary_name, c.ra, c.dec, p.orbital_period, p.orbital_period_error
     FROM exomast."Sources" AS s
-    JOIN exomast."Coords" AS c ON c.id=s.id
+    JOIN exomast."Coords" AS c ON c.id = s.id
     JOIN exomast."PlanetProperties" AS p ON p.id = s.id
-    INNER JOIN UNNEST(matched_ids) AS xid ON s.id = xid;
+    WHERE s.id IN (
+        SELECT M.id2
+        FROM exomast."Matches" AS M
+        JOIN exomast."Names" AS N ON M.id1 = N.id
+        WHERE N.name = planet_name
+    )
+    ORDER BY s.id;
 END $$ LANGUAGE plpgsql;
 
 
